2401a8e4911115671657d740ff46053e390a4981
Simon Pedro Dura <sipedura@gmail.com>
2025-03-21 11:17:39 -0300

    Se sube el ejercicio 3 de la guia 1 y se corrige el ejercicio 2 de la misma guia

--- firmware/projects/guia1_ej3/main/guia1_ej3.c
+++ firmware/projects/guia1_ej3/main/guia1_ej3.c
@@ -33,6 +33,14 @@
 #include "switch.h"
 /*==================[macros and definitions]=================================*/
 #define CONFIG_BLINK_PERIOD 1000
+
+struct leds
+{
+    uint8_t mode;       //OFF (0), ON (1), TOGGLE(3)
+	uint8_t n_led;      //indica el número de led a controlar
+	uint8_t n_ciclos; 	//indica la cantidad de ciclos de encendido/apagado
+	uint16_t periodo;   //indica el tiempo de cada ciclo
+} my_leds;
 /*==================[internal data definition]===============================*/
 
 /*==================[internal functions declaration]=========================*/
@@ -42,21 +50,86 @@ void app_main(void){
 	uint8_t teclas;
 	LedsInit();
 	SwitchesInit();
-    while(1)    {
+	struct leds sLed;
+	sLed.mode = 0;
+	sLed.n_led = 0;
+    
+	while(1)    {
     	teclas  = SwitchesRead();
     	switch(teclas){
     		case SWITCH_1:
-    			LedToggle(LED_1);
+				if(sLed.n_led < 2)
+				{
+					sLed.n_led++;
+				}
+				else 
+				{
+					sLed.n_led = 0;
+				}
     		break;
+
     		case SWITCH_2:
-    			LedToggle(LED_2);
-    		break;
-			case SWITCH_1 & SWITCH_2:
-				LedToggle(LED_3);
+				if(sLed.mode < 2)
+				{
+					sLed.mode++;
+				}
+				else 
+				{
+					sLed.mode = 0;
+				}
 			break;
 
+			case SWITCH_1 & SWITCH_2:
+				if(sLed.n_led == 0)
+				{
+					if(sLed.mode == 0)
+					{
+						LedOff(LED_1);
+					}
+					else if(sLed.mode == 1)
+					{
+						LedOn(LED_1);
+					}
+					else if(sLed.mode == 2)
+					{
+						LedToggle(LED_1);
+					}
+				}
+				else if(sLed.n_led == 1)
+				{
+					if(sLed.mode == 0)
+					{
+						LedOff(LED_2);
+					}
+					else if(sLed.mode == 1)
+					{
+						LedOn(LED_2);
+					}
+					else if(sLed.mode == 2)
+					{
+						LedToggle(LED_2);
+					}
+				}
+				else if(sLed.n_led == 2)
+				{
+					if(sLed.mode == 0)
+					{
+						LedOff(LED_3);
+					}
+					else if(sLed.mode == 1)
+					{
+						LedOn(LED_3);
+					}
+					else if(sLed.mode == 2)
+					{
+						LedToggle(LED_3);
+					}
+				}				
+			break;
     	}
 		vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
 	}
+
+	
 }
 /*==================[end of file]============================================*/
