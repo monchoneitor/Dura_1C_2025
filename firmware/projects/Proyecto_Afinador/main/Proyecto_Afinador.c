c320d082b36b463d95109b5edc412fd33332b764
monchoneitor <sipedura@gmail.com>
2025-05-23 16:01:01 -0300

    Se inicia el proyecto integrador y se agrega el ejempo DSP para usarlo como referencia

---
+++ firmware/projects/Proyecto_Afinador/main/Proyecto_Afinador.c
@@ -0,0 +1,43 @@
+/*! @mainpage Proyecto Afinador
+ *
+ * @section genDesc General Description
+ *
+ * This section describes how the program works.
+ *
+ * <a href="https://drive.google.com/...">Operation Example</a>
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 23/05/2025 | Document creation		                         |
+ *
+ * @author Simon Pedro Dura (sipedura@gmail.com)
+ * 
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "dsp"
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+void app_main(void){
+	
+}
+/*==================[end of file]============================================*/
