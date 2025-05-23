1e9be6e87c8b9d93c66a9d153cca8551b6ea7669
monchoneitor <sipedura@gmail.com>
2025-05-23 14:00:35 -0300

    Se arregla el error en el que no funcionaba la lectura del ECG.

--- firmware/projects/guia2_ej4/main/guia2_ej4.c
+++ firmware/projects/guia2_ej4/main/guia2_ej4.c
@@ -48,32 +48,23 @@
 /** @def BUFFER_SIZE
  * @brief Valor que contiene el tamaño del arreglo de caracteres que contiene el ECG muestreado.
  */
-#define BUFFER_SIZE 231
 /*==================[internal data definition]===============================*/
 /* Arreglo de caracteres que contiene valores que fueron muestrados de un ECG
  */
-const char ecg[BUFFER_SIZE] = {
-    76, 77, 78, 77, 79, 86, 81, 76, 84, 93, 85, 80,
-    89, 95, 89, 85, 93, 98, 94, 88, 98, 105, 96, 91,
-    99, 105, 101, 96, 102, 106, 101, 96, 100, 107, 101,
-    94, 100, 104, 100, 91, 99, 103, 98, 91, 96, 105, 95,
-    88, 95, 100, 94, 85, 93, 99, 92, 84, 91, 96, 87, 80,
-    83, 92, 86, 78, 84, 89, 79, 73, 81, 83, 78, 70, 80, 82,
-    79, 69, 80, 82, 81, 70, 75, 81, 77, 74, 79, 83, 82, 72,
-    80, 87, 79, 76, 85, 95, 87, 81, 88, 93, 88, 84, 87, 94,
-    86, 82, 85, 94, 85, 82, 85, 95, 86, 83, 92, 99, 91, 88,
-    94, 98, 95, 90, 97, 105, 104, 94, 98, 114, 117, 124, 144,
-    180, 210, 236, 253, 227, 171, 99, 49, 34, 29, 43, 69, 89,
-    89, 90, 98, 107, 104, 98, 104, 110, 102, 98, 103, 111, 101,
-    94, 103, 108, 102, 95, 97, 106, 100, 92, 101, 103, 100, 94, 98,
-    103, 96, 90, 98, 103, 97, 90, 99, 104, 95, 90, 99, 104, 100, 93,
-    100, 106, 101, 93, 101, 105, 103, 96, 105, 112, 105, 99, 103, 108,
-    99, 96, 102, 106, 99, 90, 92, 100, 87, 80, 82, 88, 77, 69, 75, 79,
-    74, 67, 71, 78, 72, 67, 73, 81, 77, 71, 75, 84, 79, 77, 77, 76, 76,
-};
-/* Entero de 16 bits que se utilizará para recorrer el arreglo de caracteres
+
+unsigned char ECG[] = {
+17,17,17,17,17,17,17,17,17,17,17,18,18,18,17,17,17,17,17,17,17,18,18,18,18,18,18,18,17,17,16,16,16,16,17,17,18,18,18,17,17,17,17,
+18,18,19,21,22,24,25,26,27,28,29,31,32,33,34,34,35,37,38,37,34,29,24,19,15,14,15,16,17,17,17,16,15,14,13,13,13,13,13,13,13,12,12,
+10,6,2,3,15,43,88,145,199,237,252,242,211,167,117,70,35,16,14,22,32,38,37,32,27,24,24,26,27,28,28,27,28,28,30,31,31,31,32,33,34,36,
+38,39,40,41,42,43,45,47,49,51,53,55,57,60,62,65,68,71,75,79,83,87,92,97,101,106,111,116,121,125,129,133,136,138,139,140,140,139,137,
+133,129,123,117,109,101,92,84,77,70,64,58,52,47,42,39,36,34,31,30,28,27,26,25,25,25,25,25,25,25,25,24,24,24,24,25,25,25,25,25,25,25,
+24,24,24,24,24,24,24,24,23,23,22,22,21,21,21,20,20,20,20,20,19,19,18,18,18,19,19,19,19,18,17,17,18,18,18,18,18,18,18,18,17,17,17,17,
+17,17,17
+
+} ;
+/* Entero que se utilizará para recorrer el arreglo de caracteres
  */
-uint16_t contEcg = 0;
+int contEcg = 0;
 /* Entero de 16 bits donde se almacenará el valor de tension en mV obtenidos por el CH1
  */
 uint16_t tension = 0;
@@ -130,23 +121,22 @@ static void analogInputLeer(void *pvParameter)
  * @brief Funcion que envía a travez de CH0 el valor contECG del arreglo de caracteres ecg convertido a mV
  * @return
  */
-static void analogOutputEscribir(void *pvParameter)
+void analogOutputEscribir()
 {
 	while (1)
 	{
 		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-		if(contEcg == BUFFER_SIZE)
+		if(contEcg >= 231)
 		{
 			contEcg = 0;
-			AnalogOutputWrite(ecg[contEcg]);
+			AnalogOutputWrite(ECG[contEcg]);
 			contEcg++;
 		}
-		else
+		else if(contEcg < 231)
 		{
-			AnalogOutputWrite(ecg[contEcg]);
+			AnalogOutputWrite(ECG[contEcg]);
 			contEcg++;
 		}
-			
 	}
 }
 /*==================[external functions definition]==========================*/
@@ -183,8 +173,6 @@ void app_main(void)
 		.func_p = funcionUARTRead,
 		.param_p = NULL};
 	UartInit(&pUart);
-
-
 
 	xTaskCreate(&analogInputLeer, "Medir_Analog", 1024, NULL, 5, &analogleer_task_handle);
 	xTaskCreate(&analogOutputEscribir, "Escribir_Analog", 1024, NULL, 5, &analogesc_task_handle);
