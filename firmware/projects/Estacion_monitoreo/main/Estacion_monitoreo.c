d476d74af6ef0dbd229a5fad590d86ae01a61eaa
albanopenalva <albano.penalva@uner.edu.ar>
2025-06-09 15:47:02 -0300

    Se crea la carpeta para el codigo de la estación de monitoreo

---
+++ firmware/projects/Estacion_monitoreo/main/Estacion_monitoreo.c
@@ -0,0 +1,46 @@
+/*! @mainpage Proyecto Detector humedad, temperatura y radiacion.
+ *
+ * @section genDesc General Description
+ *
+ * This section describes how the program works.
+ *
+ * <a href="https://drive.google.com/...">Operation Example</a>
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 12/09/2023 | Document creation		                         |
+ *
+ * @author Simon Pedro Dura (sipedura@gmail.ar)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "timer_mcu.h"
+#include "uart_mcu.h"
+#include "analog_io_mcu.h"
+#include "dht11_mcu.h"
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+void app_main(void){
+	printf("Hello world!\n");
+}
+/*==================[end of file]============================================*/
